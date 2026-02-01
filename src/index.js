855842e31ae0f6b5740ebfad611413b0175d4b00
Arif Billah <arifbillah8888@gmail.com>
2026-02-01 11:20:12 +0600

    Add GA4 tracking

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom/client";
 import "./assets/scss/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Modal from "react-modal";
+import ReactGA from "react-ga4";
+
+// ✅ Initialize GA4 once (top-level)
+ReactGA.initialize("G-FMGKJH1BV9");
+
+// ✅ Track page views on route changes (React Router)
+function GAListener() {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
 
 function start() {
   const rootEl = document.getElementById("root");
   if (!rootEl) {
-    // If this ever logs, your public/index.html is missing <div id="root"></div>
     console.error('Root element "#root" not found.');
     return;
   }
@@ -21,6 +38,7 @@ function start() {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
+        <GAListener />
         <App />
       </BrowserRouter>
     </React.StrictMode>
